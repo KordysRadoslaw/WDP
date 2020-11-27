423ca614fc219aba39e06c41dab9cdba02055546
KordysRadoslaw <40690745+KordysRadoslaw@users.noreply.github.com>
2020-11-27 22:10:45 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("Witam, przedstawiam moja prace domowa\n");
-    nameAndSurname();
-    nameAndLocalization();
-    ageCalculator();
-    showJanPart_1();
-    showJanPart_2();
-    showJanPart_3();
-    exponentiation();
-    smile();
+int main()
+{
+    char name[20];
+    char surname[20];
+
+    printf("Witam, podaj swoje imie: ");
+    scanf("%s", name);
+    printf("Witam, podaj swoje nazwisko: ");
+    scanf("%s", surname);
+
+    zadanie1(name, surname);
+    zadanie2(name, surname);
+    zadanie3();
+    zadanie4();
+    zadanie5(name, surname);
     return 0;
 }
-void nameAndSurname(){
-    printf("Zadanie nr 1 ->\n");
-    printf("Radoslaw Kordys\n");
-    printf("Radoslaw\nKordys\n");
-    printf("Radoslaw ");
-    printf("Kordys\n\n");
-}
-void nameAndLocalization(){
-    printf("Zadanie nr 2 ->\n");
-    printf("Radoslaw Kordys\n10-900 Olsztyn\n\n");
-}
-void ageCalculator(){
-    int age = 25;
-    int year = 365;
-    int calculate = age * year;
-    printf("Zadanie nr 3 ->\n");
-    printf("Moj wiek, czyli %d lat, w przeliczaniu na dni to: %d dni",age,calculate);
-    printf("\n\n");
-}
-void showJanPart_1(){
-    printf("Zadanie nr 4, uzyte sa dwa rozwiazania ->\n");
-    printf("Panie Janie!\n");
-    printf("Panie Janie!\n");
-}
-void showJanPart_2(){
-    printf("Rano wstan!\n\n");
-}
-void showJanPart_3(){
-    showJanPart_1();
-    showJanPart_2();
-}
-void exponentiation(){
-    printf("Zadanie nr 5 ->\n");
-    int liczba = 10;
-    int liczba2 = liczba * 2;
-    int liczba3 = liczba2 * liczba;
-    printf("%d\n%d\n%d\n",liczba,liczba2,liczba3);
-
-}
-void smile(){
-    int smile = printf("Zadanie nr 6 ->\n");
-    char smile1[20] = "Usmiech!";
-    printf("%s%s%s\n%s%s\n%s\n",smile1,smile1,smile1,smile1,smile1,smile1);
+void zadanie1(char name[], char surname[]){
+    printf("Zadanie nr 1 \n");
+    printf("\"%s %s\"\n\n", surname, name);
 }
 
+void zadanie2(char name[], char surname[]){
+    printf("Zadanie nr 2 \n");
+    int sizeOfName = strlen(name);
+    int sizeOfFreeSpace = sizeOfName + 3;
+    printf("ad a) \"%s\"\n", name);
+    printf("ad b) \"%20s\"\n", name);
+    printf("ad c) \"%-20s\"\n", name);
+    printf("ad d) \"%*s\"\n\n", sizeOfFreeSpace, name);
+}
+void zadanie3(){
+    float number;
+    printf("Zadanie nr 3 \n");
+    printf("Podaj liczbe zmiennoprzecinkowa: ");
+    scanf("%f", &number);
+    printf("ad a) Wpisano liczbe %3.1f lub liczbe %e\n",number, number);
+    printf("ad a) Wpisano liczbe %+5.4f lub liczbe %E\n\n",number, number);
+}
+void zadanie4(){
+    char name[20];
+    float height;
+    printf("Zadanie nr 4 \n");
+    printf("Podaj swoje imie: ");
+    scanf("%s", name);
+    printf("Podaj wzrost: ");
+    scanf("%f",&height);
+    printf("%s, masz %.2f metra wzrostu\n\n", name, height/100);
+}
+void zadanie5(char name[], char surname[]){
+    int sizeOfName = strlen(name);
+    int sizeOfSurname = strlen(surname);
+    printf("zadanie nr 5\n");
+    printf("%s %s\n%*d%*d\n",name, surname, sizeOfName,sizeOfName, sizeOfSurname, sizeOfSurname);
+    printf("%s %s\n%d%*d",name, surname, sizeOfName,sizeOfName, sizeOfSurname);
+}
